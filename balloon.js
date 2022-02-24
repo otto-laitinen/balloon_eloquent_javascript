d866282a74cb38d06ea54476abf809184769f743
Otto <ottolaitinen@outlook.com>
2022-02-24 20:43:55 +0200

    initial commit

---
+++ balloon.js
@@ -0,0 +1,32 @@
+const initApp = () => {
+    let balloon = document.getElementById("balloon");
+    let balloon_size = parseFloat(window.getComputedStyle(balloon, null).getPropertyValue('font-size')
+    );
+    let explosion_limit = 200;
+
+    const resize_balloon = (event) => {
+        
+        // Inflate
+        if (event.key == "ArrowUp") {
+            balloon.style.fontSize = `${balloon_size * 1.1}px`
+            balloon_size *= 1.1
+
+            // Explode
+            if (balloon_size > explosion_limit) {
+                balloon.textContent = "💥";
+                document.body.removeEventListener("keydown", resize_balloon);
+            }
+        };
+
+        // Deflate
+        if (event.key == "ArrowDown") {
+            balloon.style.fontSize = `${balloon_size * 0.9}px`
+            balloon_size *= 0.9
+        };
+    };
+
+    // Listen for key strokes
+    document.body.addEventListener("keydown", resize_balloon);
+};
+
+window.addEventListener('load', () => {initApp();});
